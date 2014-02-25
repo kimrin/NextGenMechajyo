3246a82e12519128dd235cfdb0c5bebe861b0470
Takeshi KIMURA <kittykimura@gmail.com>
2014-02-25 22:18:37 +0900

    delete Julia directory

--- Julia/Chess.jl
+++
@@ -1,8 +0,0 @@
-#!/Applications/Julia-0.3.0-prerelease-2d0e7313b3.app/Contents/Resources/julia/bin/julia
-####!/Applications/Julia-0.2.0.app/Contents/Resources/julia/bin/julia
-####!/usr/local/bin/julia 
-#
-using Mechajyo
-require("Main.jl")
-
-main(Mechajyo.Chess,ARGS)
--- Julia/USI.jl
+++
@@ -1,8 +0,0 @@
-type USI <: UGI
-    game::Uint64
-    function USI(g::Uint64)
-        game = g
-    end
-    function init()
-    end
-end
