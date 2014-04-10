1d1cba2d360439a420f6bdfa214bd8ecd5eb230b
kimrin <kittykimura@gmail.com>
2014-04-10 10:15:36 +0900

    Added minor changes, and version up.

--- src/Mechajyo.jl
+++ src/Mechajyo.jl
@@ -3,6 +3,7 @@
 module Mechajyo
 
 importall Base
+import DataStructures.Stack
 
 abstract UGI # Universal Game Interface: father of UCI, USI
 
@@ -21,7 +22,7 @@ type UCI <: UGI
     game::Uint64
     is_callback_enable::Bool
     function UCI(g::Uint64 = Chess)
-        return new(g, false)
+        new(g, false)
     end
 end
 
@@ -45,7 +46,7 @@ include("Misc.jl")
 # Constants
 
 const MECHA_JYOSHI_SHOGI = int64(2)
-const MECHAJYO_VERSION = "0.01"
+const MECHAJYO_VERSION = "0.02"
 
 const MAX_THREADS = int64(64) # Because SplitPoint::slavesMask is a uint64_t
 const MAX_SPLITPOINTS_PER_THREAD = int64(8)
